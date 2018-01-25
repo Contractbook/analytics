6f20ce25e72f7c21c21976ab320b7ad8843dd8e0
Andrew Dryga <andrew@dryga.com>
2018-01-25 08:28:09 +0200

    Expose TestClient to east testing in third-party apps

--- lib/analytics/mixpanel/test_client.ex
+++ lib/analytics/mixpanel/test_client.ex
@@ -1,4 +1,7 @@
 defmodule Analytics.Mixpanel.TestClient do
+  @moduledoc """
+  Test client that sends tracked data to the test process.
+  """
   def send_batch(endpoint_url, data) do
     send(self(), {:mixpanel_request, endpoint_url, data})
     :ok
