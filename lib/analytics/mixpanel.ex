d5297c980b5150576518bd6a0d32ff5f5e268130
Andrew Dryga <andrew@dryga.com>
2018-04-04 16:33:45 +0300

    Added alias method for Mixpanel

--- lib/analytics/mixpanel.ex
+++ lib/analytics/mixpanel.ex
@@ -2,6 +2,54 @@ defmodule Analytics.Mixpanel do
   alias Analytics.Mixpanel.{Events, People}
 
   @doc """
+  Associates one identity with another, usually an anonymous user with an identified user once they sign up.
+
+  This function MUST be called exactly once for each user when the used is signed up,
+  otherwise user would be duplicated on Mixpanel side. You MUST not submit any People data
+  before creating an alias, unless you work with legacy users that were signed up before
+  Mixpanel was integrated.
+
+  By default, DistinctID was automatically set by Mixpanel and you would want to alias it
+  with a User email in your database. (Email is preferred over User ID because your code won't
+  need to know the ID, which is useful for third-party integrations that don't have access to the
+  production data.)
+
+  Calling alias doesn't actually change Mixpanel distinct_id; instead, what it does do is add the ID to
+  a lookup table on Mixpanel’s end and map it to the original Mixpanel distinct_id.
+
+  You can receive a `disting_id` from your front-end or by reading a Mixpanel cookie set for your domain:
+
+      def client_distinct_id(conn) do
+        # Usually it's format is: "mp_\#{mixpanel_token}_mixpanel"
+        cookie_name = "mp_sldasjdlasjiu39d8ds9hfn3l_mixpanel"
+
+        with value when is_binary(value) <- get_mixpanel_cookie(conn, cookie_name),
+             value = URI.decode(value),
+             {:ok, %{"distinct_id" => distinct_id}} <- Jason.decode(value) do
+          distinct_id
+        else
+          _ -> nil
+        end
+      end
+
+      defp get_mixpanel_cookie(conn, cookie_name) do
+        maybe_fetch_cookies(conn).cookies[cookie_name]
+      end
+
+      defp maybe_fetch_cookies(%{cookies: %Plug.Conn.Unfetched{}} = conn), do: Plug.Conn.fetch_cookies(conn)
+      defp maybe_fetch_cookies(%{} = conn), do: conn
+
+  Do not batch updates to people attributes with creating an alias, alias would not be available
+  for other events and you would get duplicated in People.
+  """
+  def alias_identity(distinct_id, new_distinct_id_or_user_id) when is_binary(new_distinct_id_or_user_id) do
+    distinct_id
+    |> Events.new()
+    |> Events.track("$create_alias", %{"alias" => new_distinct_id_or_user_id})
+    |> Events.submit()
+  end
+
+  @doc """
   Tracks an event.
   """
   def track(distinct_id, event, properties \\ %{}) do
